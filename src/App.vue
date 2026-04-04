4a818ad4a2a617b39c5fcf324088cedc922dc24e
danielkatic1986 <dkatic@unipu.hr>
2026-04-04 10:34:13 +0200

    kreirao vue projekt i dodao tailwindcss

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <div class="bg-blue-500 text-white p-4">
+    <h1>Moj Vue projekt!</h1>
+    <p>Tailwind radi!</p>
+  </div>
+</template>
+
+<style scoped></style>
